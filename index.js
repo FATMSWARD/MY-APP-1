a2c5252ab853c807691fef7f2b797aa32920c0ca
FATMSWARD <138777870+FATMSWARD@users.noreply.github.com>
2023-07-06 14:32:27 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    var loading = document.getElementById('loading');
+    var mainContent = document.getElementById('main-content');
+    loading.style.display = 'none';
+    mainContent.style.display = 'block';
+  });
